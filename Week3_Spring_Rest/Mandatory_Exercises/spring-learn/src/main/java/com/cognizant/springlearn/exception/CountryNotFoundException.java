16aaf5253a461b5c8ec41064a841dc0c32c1a31c
Kollipara Jyothi Swaroop Sai <jyothiswaroopsai.kollipara@gmail.com>
2026-07-04 19:59:05 +0530

    Completed Week 3 Module 1 - Spring REST Web Services

---
+++ Week3_Spring_Rest/Mandatory_Exercises/spring-learn/src/main/java/com/cognizant/springlearn/exception/CountryNotFoundException.java
@@ -0,0 +1,13 @@
+package com.cognizant.springlearn.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class CountryNotFoundException extends RuntimeException {
+
+    public CountryNotFoundException(String message) {
+        super(message);
+    }
+
+}
