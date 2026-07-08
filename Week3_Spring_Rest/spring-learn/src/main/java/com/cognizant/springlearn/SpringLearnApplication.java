27c52f647e739495b789258e579ca419b81e3174
Kollipara Jyothi Swaroop Sai <jyothiswaroopsai.kollipara@gmail.com>
2026-07-08 07:47:07 +0530

    Week 3 Hands-on 2: Load SimpleDateFormat from Spring XML

--- Week3_Spring_Rest/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
+++ Week3_Spring_Rest/spring-learn/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -1,13 +1,40 @@
 package com.cognizant.springlearn;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @SpringBootApplication
 public class SpringLearnApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringLearnApplication.class, args);
+
+		SpringLearnApplication app = new SpringLearnApplication();
+		app.displayDate();
 	}
 
+	public void displayDate() {
+
+		try {
+
+			ApplicationContext context =
+					new ClassPathXmlApplicationContext("date-format.xml");
+
+			SimpleDateFormat format =
+					context.getBean("dateFormat", SimpleDateFormat.class);
+
+			Date date = format.parse("31/12/2018");
+
+			System.out.println(date);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
