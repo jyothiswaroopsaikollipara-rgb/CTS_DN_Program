920a8734392332b79714dacd9b4f2e85ec3a5e16
Kollipara Jyothi Swaroop Sai <jyothiswaroopsai.kollipara@gmail.com>
2026-07-08 08:11:34 +0530

    Week 3 Hands-on 6: Implement Get All Countries API

--- Week3_Spring_Rest/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
+++ Week3_Spring_Rest/spring-learn/src/main/java/com/cognizant/springlearn/controller/CountryController.java
@@ -1,11 +1,15 @@
 package com.cognizant.springlearn.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 import com.cognizant.springlearn.bean.Country;
 import com.cognizant.springlearn.service.CountryService;
 
@@ -28,5 +32,16 @@ public class CountryController {
         LOGGER.info("END");
 
         return country;
+    }
+    @GetMapping("/countries")
+    public List<Country> getAllCountries() {
+
+        LOGGER.info("START");
+
+        List<Country> countries = countryService.getAllCountries();
+
+        LOGGER.info("END");
+
+        return countries;
     }
 }
--- Week3_Spring_Rest/spring-learn/src/main/java/com/cognizant/springlearn/service/CountryService.java
+++ Week3_Spring_Rest/spring-learn/src/main/java/com/cognizant/springlearn/service/CountryService.java
@@ -1,5 +1,7 @@
 package com.cognizant.springlearn.service;
 
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
@@ -9,11 +11,17 @@ import com.cognizant.springlearn.bean.Country;
 @Service
 public class CountryService {
 
+    private ApplicationContext context =
+            new ClassPathXmlApplicationContext("country.xml");
+
     public Country getCountry() {
-
-        ApplicationContext context =
-                new ClassPathXmlApplicationContext("country.xml");
-
-        return context.getBean("country", Country.class);
+        return context.getBean("country3", Country.class);
     }
+
+    @SuppressWarnings("unchecked")
+    public List<Country> getAllCountries() {
+
+        return (List<Country>) context.getBean("countryList");
+    }
+
 }
